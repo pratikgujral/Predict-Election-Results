c9fd33e6ed5393fb6621a62a6d0071916e2c6a18
Kshitij Alwadhi <52427677+kshitijalwadhi@users.noreply.github.com>
2022-02-19 01:48:52 +0530

    clean

--- Twitter Scrape/twint_script.py
+++ Twitter Scrape/twint_script.py
@@ -4,10 +4,10 @@ from datetime import date, timedelta
 
 # HYPERPARAMETERS
 TRANSLATE_FLAG = False
-TEMPORAL_FLAG = False
+TEMPORAL_FLAG = True
 NUM_TWEETS = 1000
-since = str(datetime.datetime(2022, 2, 4))
-until = str(datetime.datetime(2022, 2, 5))
+since = str(datetime.datetime(2022, 2, 15))
+until = str(datetime.datetime(2022, 2, 17))
 
 
 def scrape_tweets_from_query(query,fname):
@@ -33,8 +33,8 @@ def scrape_tweets_from_user(username,fname):
     if TRANSLATE_FLAG:
         config.Translate = True
         config.TranslateDest = "en"
-    else:
-        config.Lang = "en"
+    # else:
+    #     config.Lang = "en"
     config.Hide_output = True
     config.Limit = NUM_TWEETS
     if TEMPORAL_FLAG:
@@ -44,22 +44,8 @@ def scrape_tweets_from_user(username,fname):
     config.Output = fname
     twint.run.Search(config)
 
-def scrape_replies_on_tweet(tweet_id,fname):
-    config = twint.Config()
-    config.Tweet_id = tweet_id
-    if TRANSLATE_FLAG:
-        config.Translate = True
-        config.TranslateDest = "en"
-    else:
-        config.Lang = "en"
-    config.Get_replies = True
-    config.Store_csv = True
-    config.Output = fname
-    twint.run.Search(config)
 
 
 if __name__ == "__main__":
-    #scrape_tweets_from_query("covid", "tweets_covid19.csv")
     #scrape_tweets_from_user("AmitShah", "tweets_amit.csv")
-    #scrape_replies_on_tweet("1489956615902343177", "tweets_replies.csv")
     scrape_tweets_from_query("aam aadmi party", "tweets_aam_aadmi.csv")
