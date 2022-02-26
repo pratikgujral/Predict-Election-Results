cb6a239d6ff612dbf74b7bacfdefbfef8d8f6e80
Kshitij Alwadhi <52427677+kshitijalwadhi@users.noreply.github.com>
2022-02-26 21:08:28 +0530

    add script to combine test data

---
+++ Datasets/fetch_test_data.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+data_source = "../Twitter Scrape/data/"
+output_path = "Testing/"
+categories = ["aap","bsp","bjp","general","inc","samajwadi"]
+
+for category in tqdm(categories):
+    path = data_source + category + "/"
+    files = os.listdir(path)
+    query_df_map = {}
+    for file in files:
+        query = file.split("2022")[0]
+        if query[-1]=="_":
+            query = query[:-1]
+        df = pd.read_csv(path + file)
+        if query not in query_df_map:
+            query_df_map[query] = df
+        else:
+            query_df_map[query] = pd.concat([query_df_map[query],df])
+    for query in query_df_map:
+        query_df_map[query]["query"] = query
+    df = pd.DataFrame()
+    for query in query_df_map:
+        df = pd.concat([df,query_df_map[query]])
+    df.to_csv(output_path + category + ".csv",index=False)
+
+
