a24220359ca148fbad2c7391bb99a213d5faebf1
Kshitij Alwadhi <52427677+kshitijalwadhi@users.noreply.github.com>
2022-02-26 20:38:48 +0530

    update files

--- Twitter Scrape/twint_script.py
+++ Twitter Scrape/twint_script.py
@@ -10,7 +10,7 @@ NUM_TWEETS = 20000
 since = str(date.today() - timedelta(days=1))
 until = str(date.today())
 
-bjp_search_list_1 = ["bjp","@BJP4India","@narendramodi","#BJPwinningUP","@AmitShah"]
+bjp_search_list = ["bjp","@BJP4India","@narendramodi","#BJPwinningUP","@AmitShah"]
 bjp_search_list_2 = ["@myogiadityanath","yogi","@BJP4TamilNadu","@BJP4UP","@JPNadda","@CHARANJITCHANNI","channi"]
 aap_search_list = ["aam aadmi party","#AAP","@AamAadmiParty","kejriwal","#ArvindKejriwal","@AAPPunjab","#KejriwalVsAll","@ArvindKejriwal","#AAPdePaap","@msisodia","@BhagwantMann"]
 inc_search_list = ["@INCIndia","@RahulGandhi","Rahul Gandhi","@INCPunjab","@priyankagandhi","@INCUttarPradesh"]
@@ -52,21 +52,26 @@ def scrape_tweets_from_user(username,fname):
 
 
 if __name__ == "__main__":
-    for query in tqdm(bjp_search_list_1):
+    for query in tqdm(inc_search_list):
+        fname = "data/inc/" + query + "_" + str(date.today()) + ".csv"
+        scrape_tweets_from_query(query,fname)
+    for query in tqdm(general_search_list):
+        fname = "data/general/" + query + "_" + str(date.today()) + ".csv"
+        scrape_tweets_from_query(query,fname)
+    for query in tqdm(bsp_search_list):
+        fname = "data/bsp/" + query + "_" + str(date.today()) + ".csv"
+        scrape_tweets_from_query(query,fname)
+    for query in tqdm(samajwadi_search_list):
+        fname = "data/samajwadi/" + query + "_" + str(date.today()) + ".csv"
+        scrape_tweets_from_query(query,fname)
+    for query in tqdm(bjp_search_list_2):
         fname = "data/bjp/" + query + "_" + str(date.today()) + ".csv"
         scrape_tweets_from_query(query,fname)
-    # for query in inc_search_list:
-    #     fname = "data/inc/" + query + "_" + str(date.today()) + ".csv"
-    #     scrape_tweets_from_query(query,fname)
+
+
     # for query in aap_search_list:
     #     fname = "data/aap/" + query + "_" + str(date.today()) + ".csv"
     #     scrape_tweets_from_query(query,fname)
-    # for query in general_search_list:
-    #     fname = "data/general/" + query + "_" + str(date.today()) + ".csv"
-    #     scrape_tweets_from_query(query,fname)
-    # for query in bsp_search_list:
-    #     fname = "data/bsp/" + query + "_" + str(date.today()) + ".csv"
-    #     scrape_tweets_from_query(query,fname)
-    # for query in samajwadi_search_list:
-    #     fname = "data/samajwadi/" + query + "_" + str(date.today()) + ".csv"
+    # for query in bjp_search_list:
+    #     fname = "data/bjp/" + query + "_" + str(date.today()) + ".csv"
     #     scrape_tweets_from_query(query,fname)
