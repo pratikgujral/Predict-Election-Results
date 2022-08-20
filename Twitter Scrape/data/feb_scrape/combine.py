863e073690299b5be3fed1473821fc8c4537719c
navya19j <navya19j@gmail.com>
2022-08-20 15:19:49 +0530

    update categorization

--- Twitter Scrape/data/feb_scrape/combine.py
+++ Twitter Scrape/data/feb_scrape/combine.py
@@ -2,7 +2,12 @@ import os
 import pandas as pd
 from tqdm import tqdm
 
-#combine all json files in a folder
+#combine all json files in a folder into multiple json files
+folder = "data/feb_scrape/bjp/"
+file_bjp_up = "data/feb_scrape/bjp/bjp_up.json"
+file_bjp_punjab = "data/feb_scrape/bjp/bjp_punjab.json"
+file_bjp_rest = "data/feb_scrape/bjp/bjp_rest.json"
+
 def combine(folder, fname):
     files = os.listdir(folder)
     combined = []
